9b7e1293d09f191deae945f6d9a823e7102c61e8
Phebueno <fcbgomes@gmail.com>
2023-01-21 02:21:54 -0300

    implementada entrada na sala

--- assets/js/chatuol.js
+++ assets/js/chatuol.js
@@ -1,19 +1,36 @@
 //Função que pega o nome do usuário
-function entrarNaSala(){
+function login(){     
     const nome = prompt("Insira seu nome para o login:");
-    return nome;
-    }
-//Funções que retornam status do login, periodicamente
+    objUser={
+        name:nome
+    }; 
+    //Envia ao server
+    const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',objUser);
+    requisicao.then(loginSuccess);
+    requisicao.catch(loginFail);       
+}
+//Funções que ocorrem após login, periodicamente
 function loginSuccess(requisicao){
     console.log('Logado!');
-    console.log(requisicao);
+    console.log(objUser);
+    //Checa usuário a cada 5 segundos
+    setInterval(function () {
+    let verificacao = axios.post('https:/xx/mock-api.driven.com.br/api/v6/uol/status',objUser);
+    verificacao.catch(desconectado);}
+    , 5000);
+    //Solicita ao servidor msgs a cada 3 segundos.
+    setInterval(function () {
+    let buscarMensagem = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    buscarMensagem.then(msgSucesso);
+    buscarMensagem.catch(msgFail);}
+    , 3000);    
 }
 
 function loginFail(requisicao){
-    console.log('ixi, deu ruim');
+    alert('Este nome não está disponível, por favor, tente novamente.');    
     console.log(requisicao.response);
+    login(); //recursividade no login!
 }
-
 //Retornam as mensagens, periodicamente
 
 function msgSucesso(msgs){
@@ -25,42 +42,28 @@ function msgFail(msgs){
     console.log('Não achamos nada :(');
     console.log(msgs.response);
 }
-
+//Funções que seguem após verificação de 5 segundos
+function desconectado(requisicao){
+    alert('Você foi desconectado! A página irá atualizar automaticamente.');   
+    console.log(requisicao.response);
+    window.location.reload();
+}
 //Objeto que deve ser enviado ao servidor
-let login={
-    name:entrarNaSala()
-};
-//Envia ao server
-const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',login);
-requisicao.then(loginSuccess);
-requisicao.catch(loginFail);
-
-//Checa usuário a cada 5 segundos
-setInterval(function () {
-    let verificacao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status',login);
-    verificacao.then(loginSuccess);
-    verificacao.catch(loginFail);}
-    , 5000);
-
-//Solicita ao servidor msgs a cada 3 segundos
-setInterval(function () {
-    let buscarMensagem = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    buscarMensagem.then(msgSucesso);
-    buscarMensagem.catch(msgFail);}
-    , 3000);
+let objUser = {};
+login();
 
 function atualizarChat(msgs){
     const elemento = document.querySelector('.mensagens');
     elemento.innerHTML = "";
     //Encontra todas as mensagens:
     msgs.forEach(msg => {
-        if (msg.type==="status") 
+        if (msg.type ==="status") 
             elemento.innerHTML += `<div class="mbox status">
             <span class="horamsg">${msg.time}</span>
             <span class="pessoa remetente">${msg.from}</span>
             <span class="mensagem">${msg.text}</span>
         </div>`
-        else if (msg.type==="message") {
+        else if (msg.type ==="message") {
             elemento.innerHTML += `<div class="mbox">
             <span class="horamsg">${msg.time}</span>
             <span class="pessoa remetente">${msg.from}</span>
@@ -70,7 +73,7 @@ function atualizarChat(msgs){
         </div>`
         }
         //FALTA ADICIONAR CONDIÇÕES DENTRO DA MENSAGEM PRIVADA
-        else if (msg.type==="private_message") {
+        else if (msg.type ==="private_message") {
             elemento.innerHTML += `<div class="mbox reservado">
             <span class="horamsg">${msg.time}</span>
             <span class="pessoa remetente">${msg.from}</span>
@@ -82,4 +85,8 @@ function atualizarChat(msgs){
         }
         elemento.lastChild.scrollIntoView();
     });    
+}
+
+function enviarMensagem(){
+    
 }
